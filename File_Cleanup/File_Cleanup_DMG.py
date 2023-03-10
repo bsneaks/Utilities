5b326955327027bd9ba74d69d93dd68afa3beae1
bsneaks <blb552000@gmail.com>
2023-03-09 18:35:20 -0800

    housekeeping

---
+++ File_Cleanup/File_Cleanup_DMG.py
@@ -0,0 +1,14 @@
+import os
+
+#set directory path
+directory = '/Users/brianbaseggio/Downloads'
+
+#set file extension to delete
+file_extension = '.dmg'
+
+#Traverse directory to search for and delete defined file type
+for root, dirs, files, in os.walk(directory):
+    for file in files:
+        if file.endswith(file_extension):
+            file_path = os.path.join(root, file)
+            os.remove(file_path)
---
+++ Readme.md
@@ -0,0 +1,4 @@
+Collection of simple scripts to help with EveryDay Life. 
+
+File_Cleanup_CSV.py Is a utility to delete any .csv files within your ~/Downloads folder
+File_Cleanup.DMG.py Is a utility to delete any .DMG files within your ~/Downloads folder
